30be240bf48ef6d33c17993f5cfbab7e380a18b1
Patrick Gundlach <gundlach@speedata.de>
2024-08-15 11:13:51 +0200

    Basic output working again

--- document/document.go
+++ document/document.go
@@ -1,83 +1,68 @@
 package document
 
 import (
+	"github.com/boxesandglue/boxesandglue/backend/bag"
 	"github.com/boxesandglue/boxesandglue/frontend"
 	"github.com/boxesandglue/csshtml"
+	"github.com/boxesandglue/htmlbag"
 )
 
 // Document is the main starting point of the PDF generation.
 type Document struct {
-	Title    string
-	Author   string
-	Keywords string // separated by comma
-	Creator  string
-	Subject  string
-	Frontend *frontend.Document
+	Title      string
+	Author     string
+	Keywords   string // separated by comma
+	Creator    string
+	Subject    string
+	Frontend   *frontend.Document
+	cssbuilder *htmlbag.CSSBuilder
 }
 
-// // PageSize returns a struct with the dimensions of the current page.
-// func (d *Document) PageSize() (cssbuilder.PageDimensions, error) {
-// 	return d.cssbuilder.PageSize()
-// }
+// PageSize returns a struct with the dimensions of the current page.
+func (d *Document) PageSize() (htmlbag.PageDimensions, error) {
+	return d.cssbuilder.PageSize()
+}
 
-// // ReadCSSFile parses the CSS file at the filename.
-// func (d *Document) ReadCSSFile(filename string) error {
-// 	return d.cssbuilder.ReadCSSFile(filename)
-// }
+// ReadCSSFile parses the CSS file at the filename.
+func (d *Document) ReadCSSFile(filename string) error {
+	return d.cssbuilder.ReadCSSFile(filename)
+}
 
-// // AddCSS permanently adds the css instructions to the current state.
-// func (d *Document) AddCSS(css string) {
-// 	d.cssbuilder.AddCSS(css)
-// }
+// OutputAt writes the HTML string to the PDF.
+func (d *Document) OutputAt(html string, width bag.ScaledPoint, x, y bag.ScaledPoint) error {
+	if err := d.cssbuilder.InitPage(); err != nil {
+		return err
+	}
 
-// // HTMLToText interprets the HTML string and applies all previously read CSS data.
-// func (d *Document) HTMLToText(html string) (*frontend.Text, error) {
-// 	return d.cssbuilder.HTMLToText(html)
-// }
+	te, err := d.cssbuilder.HTMLToText(html)
+	if err != nil {
+		return err
+	}
+	vl, err := d.cssbuilder.CreateVlist(te, width)
+	if err != nil {
+		return err
+	}
+	p := d.Frontend.Doc.CurrentPage
+	p.OutputAt(x, y, vl)
+	return nil
+}
 
-// // ParseHTMLFromNode interprets the HTML structure and applies all previously read CSS data.
-// func (d *Document) ParseHTMLFromNode(html *html.Node) (*frontend.Text, error) {
-// 	return d.cssbuilder.ParseHTMLFromNode(html)
-// }
-
-// // OutputAt writes the HTML string to the PDF.
-// func (d *Document) OutputAt(html string, width bag.ScaledPoint, x, y bag.ScaledPoint) error {
-// 	if err := d.cssbuilder.InitPage(); err != nil {
-// 		return err
-// 	}
-
-// 	te, err := d.cssbuilder.HTMLToText(html)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	err = d.cssbuilder.OutputAt(te, x, y, width)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// // ShowCSS dumps the currently known CSS to a CSS like string
-// func (d *Document) ShowCSS() string {
-// 	return d.cssbuilder.ShowCSS()
-// }
-
-// NewWithFrontend creates a document with a boxes and glue frontend document.
-func NewWithFrontend(fe *frontend.Document, cssparser *csshtml.CSS) *Document {
+// NewWithFrontend creates a document from a boxes and glue frontend document
+// and a csshtml CSS parser. The default fonts for the families monospace, sans
+// and serif are loaded.
+func NewWithFrontend(fe *frontend.Document, cssparser *csshtml.CSS) (*Document, error) {
 	d := &Document{}
+	var err error
+	d.cssbuilder, err = htmlbag.New(fe, cssparser)
+	if err != nil {
+		return nil, err
+	}
 	d.Frontend = fe
-	// d.cssbuilder = cssbuilder.New(fe, cssparser)
-	return d
+	return d, nil
 }
 
-// // CreateVlist returns a single vertical list ready to be placed in the PDF.
-// func (d *Document) CreateVlist(te *frontend.Text, wd bag.ScaledPoint) (*node.VList, error) {
-// 	return d.Frontend.CreateVlist(te, wd)
-// }
-
-// New writes the PDF
+// New creates a PDF file with the provided file name, initializes a new boxes
+// and glue frontend document and loads a default CSS stylesheet.
 func New(filename string) (*Document, error) {
 	var err error
 	d := &Document{}
@@ -85,16 +70,8 @@ func New(filename string) (*Document, error) {
 	if err != nil {
 		return nil, err
 	}
-	if err = d.Frontend.LoadIncludedFonts(); err != nil {
-		return nil, err
-	}
-	// d.cssbuilder = cssbuilder.New(d.Frontend, csshtml.NewCSSParserWithDefaults())
-
-	if d.Frontend.Doc.DefaultLanguage, err = frontend.GetLanguage("en"); err != nil {
-		return nil, err
-	}
-	d.Frontend.Doc.CompressLevel = 9
-	return d, nil
+	cs := csshtml.NewCSSParserWithDefaults()
+	return NewWithFrontend(d.Frontend, cs)
 }
 
 // Finish writes and closes the PDF.
--- go.mod
+++ go.mod
@@ -1,22 +1,25 @@
 module github.com/boxesandglue/bagme
 
-go 1.18
+go 1.21
+
+toolchain go1.22.0
 
 require (
-	github.com/boxesandglue/boxesandglue v0.0.0-20240707172207-734d018812d1
-	github.com/boxesandglue/csshtml v0.0.0-20240707131722-d3420db0de12
+	github.com/boxesandglue/boxesandglue v0.0.0-20240815090256-4262a0b934f0
+	github.com/boxesandglue/csshtml v0.0.0-20240813134317-f214983f48f8
+	github.com/boxesandglue/htmlbag v0.0.0-20240815091018-8d9a758ef2e8
 )
 
 require (
-	github.com/PuerkitoBio/goquery v1.9.1 // indirect
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
-	github.com/boxesandglue/baseline-pdf v0.0.0-20240707171747-eb4f68a9505a // indirect
+	github.com/boxesandglue/baseline-pdf v0.0.0-20240805104743-f940543238d5 // indirect
 	github.com/boxesandglue/gofpdi v1.0.19 // indirect
-	github.com/boxesandglue/textlayout v0.0.0-20240707131722-d155d7ceedcd // indirect
-	github.com/google/uuid v1.3.0 // indirect
+	github.com/boxesandglue/textlayout v0.0.0-20240805104743-4ef76f4729dd // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/speedata/css/scanner v0.0.0-20240704081300-00e8c453c7d5 // indirect
-	github.com/speedata/hyphenation v1.0.1 // indirect
-	golang.org/x/image v0.7.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/speedata/hyphenation v1.0.2 // indirect
+	golang.org/x/image v0.19.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 )
